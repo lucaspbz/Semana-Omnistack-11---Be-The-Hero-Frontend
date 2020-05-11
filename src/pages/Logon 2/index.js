0214e1c67df4745540e4fd2a2825b9a4abc5ed49
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-05-11 16:25:26 -0300

    correcting bug with git

--- src/pages/Logon 2/index.js
+++ src/pages/Logon 2/index.js
@@ -21,7 +21,7 @@ export default function Logon() {
 
             localStorage.setItem('ongId', id);
             localStorage.setItem('ongName', response.data.name);
-            history.push('/profile')
+            history.push('/profile');
         } catch (error) {
             alert('Falha no login, tente novamente.')
         }
