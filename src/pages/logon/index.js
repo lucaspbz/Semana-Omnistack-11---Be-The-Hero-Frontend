96440d1248726d0b79abec739ff990ad53557a76
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-05-11 13:32:19 -0300

    Configuring routes and starting "Axios" config

--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css'
 
-import heroesImg from '../../assets/2532522.jpg'
+import heroesImg from '../../assets/pessoas.png'
 import logoImg from '../../assets/logo.png'
 
 export default function Logon() {
@@ -17,9 +18,9 @@ export default function Logon() {
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
 
-                    <a href="/register">
+                    <Link className='back-link' to="/register">
                         <FiLogIn size={16} color="#E02041"/>
-                        Não tenho cadastro</a>
+                        Não tenho cadastro</Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes" />
