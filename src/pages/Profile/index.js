25c677c84e3faca217bf4c100fc43a14f93bb5f5
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2020-05-11 14:41:13 -0300

    Logout function

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import api from '../../services/api';
 
 import './styles.css'
@@ -11,6 +11,7 @@ import logoImg from '../../assets/logo.png';
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
 
+    const history = useHistory();
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
 
@@ -39,6 +40,12 @@ export default function Profile() {
 
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -46,7 +53,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
